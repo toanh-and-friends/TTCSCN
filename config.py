7b7e67baee49d758dd996910274da9df9825d341
HieuPhamBa <45411070+HieuPhamBa@users.noreply.github.com>
2020-12-30 14:27:07 +0700

    ssl config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 TOKEN = '1414105062:AAGczNbvxlmpm0QV4M7GnkJiI5-fyrmty34'
-NGROK_URL = 'https://ttcscn.tk/chatbot'
+#NGROK_URL = 'https://ttcscn.tk/chatbot'
+SITE_URL = 'https://40.117.140.73:8081/'
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
-LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
+LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(SITE_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
 TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id={}&text={}'
 TELEGRAM_GET_PHOTO_INFO_URL = BASE_TELEGRAM_URL + '/getFile?file_id={}'
